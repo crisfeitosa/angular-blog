da3ea0685cb91409d8639f819f25d5654100aef4
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-13 15:00:58 -0300

    feat: remove MenuBar import from Home component for cleaner structure

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuTitle } from "../../components/menu-title/menu-title";
-import { MenuBar } from "../../components/menu-bar/menu-bar";
 import { BigCard } from "../../components/big-card/big-card";
 import { SmallCard } from "../../components/small-card/small-card";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitle, MenuBar, BigCard, SmallCard],
+  imports: [MenuTitle, BigCard, SmallCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
